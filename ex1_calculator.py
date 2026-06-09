813ff52683b1e8acefec35a76d7de150adb12488
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-09 08:19:44 +0530

    Add Calculator Program

---
+++ ex1_calculator.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+print(" by adding 10 with 20 we get = ", + (a + b))
+print(" by subtracting 10 from 20 we get = ", + (a - b))
+print(" by dividing 10 with 20 we get = ", +  (a / b))
+print(" by multiplying 10 with 20 we get = ", + (a * b))
+print(" by doing modulus of 10 and 20 we get = ", + (a % b))
+print(" by doing floor division of 10 and 20 we get = ", + (a // b))
+print(" by doing exponential of 10 with 20 we get = ", + (a ** b))
+
+print(" this is the calculator i made using the 3 days knowledge", end=" by shashwat on 9th june 2026") 
