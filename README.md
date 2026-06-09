9ef1140fe4e35e469e86edd758412db508a60317
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-09 06:20:45 +0530

    Add Day 1 Python learning

---
+++ README.md
@@ -0,0 +1,5 @@
+python leaning 
+# day 1 
+Topics Learned 
+escape sequence , print function and seperate and end parameters
+date 8 junes
---
+++ day1_python_learning.py
@@ -0,0 +1,27 @@
+# day 1 
+# escape sequence and print function
+# date 6 june 2026 by shashwat
+
+# 1. escape sequence
+
+# for adding new line in a string we use \n
+
+print("hey there \n how are you?")
+# now it will create a new line after hey there and print how are you in the next line
+
+# for adding double quote in a string we use \"
+print("he said \"hello\" to me yesterday")
+# now it will print he said "hello" to me yesterday
+
+# 2. seperator and end parameter in print function
+
+# we can seperate any values in print function by using sep="anything""
+
+print("hello","world","how are you?",sep="-")
+# now it will print hello-world-how are you?
+
+# 3 End parameter in print function
+
+# we can end the print statement with anything we want by using end="anything"
+
+print("and this is how i end my first day of python learning", end=" by shashwat")
