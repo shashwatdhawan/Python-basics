06499bfe0ab6f8bbf50ff9dda329b79952550ada
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-09 07:01:49 +0530

    Add Day 2 Variables

---
+++ day2_variables.py
@@ -0,0 +1,49 @@
+# day 2 of python learning
+# Variables and data types
+# by shashwat on 9th june 2026
+
+# variables are used to store data in a program.
+# example:
+a = 10
+b = 20
+print(a + b)
+# variables are of different types like:
+
+# 1. int - for whole numbers
+# example:
+a = 15
+b = 30
+print(a * b)
+
+# 2. float - for decimal numbers
+# example:
+e = 3.14
+g = 9.8
+print(e / g)
+
+# 3. str - for strings (text) here we use "text" or 'text' to define a string
+# example:
+name = "shashwat"
+print("hello " + name)
+
+# 4. bool - for values that can be true or false
+# example:
+someone_18_or_older_drive = True
+print(someone_18_or_older_drive)
+
+# 5. list - for collection of items in a program
+# example:
+list1 = ["apple", "banana", "guava", 6 , 7.5, ]
+print(list1)
+
+# 6. tuple - for collection of items in a program that cannot be changed (immutable)
+# example:
+tuple1 = ("apple", "banana", "guava", )
+print(tuple1)
+
+# 7. dict - for collection of key-value pairs
+# example:
+dict1 = {"name": "shashwat", "age": 20, "city": "chandigarh"}
+print(dict1)
+
+print(" this is all i learned in day 2 of python leaning, variables and data types", end= "by shashwat on 9th june 2026")
