19c342e7aa506ac6cff492867a9c94c6731176eb
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-09 07:20:51 +0530

    Add Day 3 Operators

---
+++ day3_operators.py
@@ -0,0 +1,29 @@
+# day 3 of python learning
+# by shashwat on 9th june 2026
+
+# operators in python
+
+# 1. Addition operator ( a + b )
+a = 10
+b = 15
+print(a + b)
+
+# 2. subtraction operator ( a - b )
+print(a - b)
+
+# 3. multiplication operator ( a * b )
+print(a * b)
+
+# 4. division operator ( a / b )
+print(a / b)
+
+# 5. modulus operator ( a % b )
+print(a % b)
+
+# 6. floor division operator ( a // b )
+print(a // b)
+
+# 7. exponentiation operator ( a ** b )
+print(a ** b)
+
+print("these are the basic operators i learned today", end=" by shashwat on 9th june 2026")
