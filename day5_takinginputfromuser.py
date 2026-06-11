2b74145385cfe2fbe2aea79e271a0346b930c2c3
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-11 18:54:11 +0530

    Add day 5: Taking inputs from user

---
+++ day5_takinginputfromuser.py
@@ -0,0 +1,16 @@
+# day 5 of python learning 
+# taking inputs from user
+# by shashwat on 11 june 2026
+
+# to take input from user we use input() function
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+# we can also take input as numbers
+x = input("enter a number: ")
+y = input(" enter another number: ")
+
+print(int(x) + int(y))  # we need to convert the input to integer before adding
+
+# this is all i have learnt about taking input from user in python. 
+print(" by shashwat.. on 11th june 2026")
