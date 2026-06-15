093ab9d9e2b48c38efceecaa9fc9ce9a5278bb2b
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-15 23:17:35 +0530

    Add day6 strings basic

---
+++ day6_strings.py
@@ -0,0 +1,18 @@
+# day 6 string in python
+# by shashwat on 13th june 2026
+
+# string
+a = "hello"
+b = 'world'
+c = """ this is a multi line string"""
+print(a + b + c)
+print(a[1])
+print(c[15])
+st = '''
+1,440,774 views  Dec 8, 2022  Python for Beginners (Full Course) | #100DaysOfCode Programming Tutorial '''
+
+for character in st:
+    print(character )
+
+# this is the string basics in python 
+# on 15th june 2026
