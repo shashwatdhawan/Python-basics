e9a1e1d87db4aa8883c0bda5ddfc43baba8228df
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-06-15 23:49:09 +0530

    Add day7 string slices

---
+++ day7_stringslices.py
@@ -0,0 +1,15 @@
+# day 7 of python learning
+# string slices 
+# on 15th june 2026
+
+a = "Hello, World!"
+alen = len(a)
+print(a[:6]) # output: Hello as it counts 0 but not includes 6 
+print(a[-5:-2]) # this will count from len(a) - 5 to len(a) -2 and give us results 
+
+# Quick quiz 
+nm = "harry"
+print(nm[-4:-2]) 
+# this will do len(nm) - 4 to len(nm) -2 and the answer will be ar 
+
+# this is all for string slices on june 15th 2026
