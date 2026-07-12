c7cb552f00979a300f67c58f28b9f8463aee44fd
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-07-12 15:15:44 +0530

    Add day12 While-loop

---
+++ day12_while-loop.py
@@ -0,0 +1,18 @@
+# # day 12 of while loop in python 
+# i = 0
+# while( i <= 3):
+#     print(i)
+#     i = i + 1
+
+# i = 1
+# while( i <= 40):
+#     i = int(input("Enter a number: "))
+#     print(i)
+
+# print("The loop has ended.")
+
+name = input("Enter your name: ")
+i = 5
+while(i > 0):
+    print("Hello " + name)
+    i = i - 1
