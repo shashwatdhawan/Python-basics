4357eb6c5f504b0ce2f3002a7f0ff0743e777e00
shashwatdhawan <shashwatdhawan747@gmail.com>
2026-07-15 21:06:38 +0530

    Add Day 14: Functions Basics

---
+++ day14_functions.py
@@ -0,0 +1,23 @@
+# in this file we will learn about function in python
+# functions are used in python shaort the length of code and if we want to use something repeatedly we can use function to avoid writing the same code again and again
+def greet(name):
+    print("hello " + name + " Good morning")
+    # now we will call the function
+
+greet("Shashwat")
+
+# we can also use functions for calculations and other tasks like
+def divide(a, b):
+    dent = a / b
+    print(dent)
+    
+
+divide(10, 2)
+
+def endingline():
+    end = print("This is all i have done for today , see you tommorow ")
+    print(end) 
+
+endingline() 
+    
+
